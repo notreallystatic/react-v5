a20cf3d07727e8fefbffd886925e57a55b6a6a47
Sachin Kumar <sk92907@gmail.com>
2020-06-27 10:42:31 +0530

    ErrorBoundary

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,13 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
+    // throw new Error("lol");
     // Useful for things like AJAX requests
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -41,4 +43,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
